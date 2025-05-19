c1a933e8eb8fae639d122578eab916e780a9858d
Noe Prat <117754404+noeprat@users.noreply.github.com>
2025-05-19 14:27:19 +0200

    update pref dataset

--- utils/generate_preference_dataset.py
+++ utils/generate_preference_dataset.py
@@ -46,3 +46,56 @@ def generate_trajectories(policy, n_trajectories, env, max_t=int(1000), seed=0, 
         if traj_index % print_every == 0:
             print('Trajectory: ', traj_index)
     return trajectories_rewards, trajectories_states, trajectories_actions
+
+def generate_preference_dataset(pi1, pi2, dataset_size, env, max_t=int(999), seed=0, dim_state=4, print_every=10, sampling_method='pi1_pi2_trajectories'):
+    """
+    Generate a preference dataset of size n_trajectories*(max_t+1), using pi1 and pi2. Careful, dataset_size must be a multiple of (max_t+1)
+    Arguments:
+    pi1, 
+    pi2, 
+    dataset_size, 
+    env, 
+    max_t=int(1000), 
+    seed=0, 
+    dim_state=4, 
+    print_every=10,
+    sampling_method, str for how the states are sampled
+
+    Returns:
+    states, np.array(dataset_size, dim_state)
+    preferred_actions, np.array(dataset_size)
+    rejected_actions, np.array(dataset_size)
+    """
+    n_trajectories = dataset_size // (max_t+1)
+    states = np.zeros((dataset_size, dim_state))
+    preferred_actions = np.zeros(dataset_size)
+    rejected_actions = np.zeros(dataset_size)
+
+    if sampling_method == 'pi1_pi2_trajectories':
+
+        print("\n First half \n")
+        _, trajectories_states, trajectories_actions = generate_trajectories(pi1, n_trajectories//2, env, max_t, seed, dim_state, print_every)
+        for traj_index in range(n_trajectories//2):
+            for t in range(max_t+1):
+                states[traj_index*(int(max_t)+1)+t,:] = trajectories_states[traj_index, t, :]
+                preferred_actions[traj_index*(max_t+1)+t] = trajectories_actions[traj_index, t]
+                rejected_actions[traj_index*(max_t+1)+t] = pi2.act(trajectories_states[traj_index,t])[0]
+
+        print("\n Second half \n")
+        _, trajectories_states, trajectories_actions = generate_trajectories(pi2, n_trajectories//2, env, max_t, seed, dim_state, print_every)
+        for traj_index in range(n_trajectories//2):
+            for t in range(max_t+1):
+                states[(traj_index+n_trajectories//2)*(max_t+1)+t,:] = trajectories_states[traj_index, t,:]
+                preferred_actions[traj_index*(max_t+1)+t] = pi1.act(trajectories_states[traj_index,t])[0]
+                rejected_actions[traj_index*(max_t+1)+t] = trajectories_actions[traj_index, t]
+    elif sampling_method == 'uniform_cartpole':
+        states[:,0] = np.random.uniform(low= -4.8, high= 4.8, size=dataset_size)
+        states[:,1] = np.random.standard_normal(dataset_size)
+        states[:,2] = np.random.uniform(low= -0.418, high= 0.418, size=dataset_size)
+        states[:,3] = np.random.standard_normal(dataset_size)
+    
+        preferred_actions = np.array([pi1.act(state)[0] for state in states])
+        rejected_actions = np.array([pi2.act(state)[0] for state in states])
+
+
+    return states, preferred_actions, rejected_actions
